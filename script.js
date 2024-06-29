b23072b3274213acd69237d859a5e944e290c524
AndyofStateFarm <andrewjb97@yahoo.com>
2024-06-28 21:17:32 -0400

    Implement error handling for data fetching

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-function call()
-{
-    const request = fetch(`https://course-api.com/react-store-products`).then((response)=>{
-        return response.json();
-    }).then((data)=>{
-        document.querySelector("#productName").textContent = `${data[0].name}`;
-        document.querySelector("#productPicture").src = `${data[0].image}`;
-        document.querySelector("#productPrice").textContent = `${data[0].price}`;
-        document.querySelector("#productDescription").textContent = `${data[0].description}`;
+// U08282838
+
+document.addEventListener('DOMContentLoaded', () => {
+    let count = 0;
+    function call(count)
+    {
+        const request = fetch(`https://course-api.com/react-store-products`).then((response)=>{
+            return response.json();
+        }).then((data)=>{
+            console.log(data);
+            document.querySelector("#productName").textContent = `${data[count].name}`;
+            document.querySelector("#productPicture").src = `${data[count].image}`;
+            document.querySelector("#productPrice").textContent = `$${data[count].price}`;
+            document.querySelector("#productDescription").textContent = `${data[count].description}`;
+        }).catch((err)=>{
+            console.log(err);
+            alert('There was an error loading products. Please try again soon.')  
+        }).finally(()=>{
+            console.log("Nothing to see here");
+        })
+    }
+
+        call(count)
+
+
     })
-}
-call()
