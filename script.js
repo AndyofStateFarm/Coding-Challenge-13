2cacba61192f665b4bcdf7fc33340c3d254a597e
AndyofStateFarm <andrewjb97@yahoo.com>
2024-06-28 22:36:22 -0400

    Style application and add smooth transitions.

--- script.js
+++ script.js
@@ -14,14 +14,16 @@ document.addEventListener('DOMContentLoaded', () => {
         const request = await fetch(`https://course-api.com/react-store-products`).then((response)=>{
             return response.json();
         }).then((data)=>{
-            Loading.style.display = `none`
             console.log(data);
             countlength = data.length;
             console.log(countlength);
             document.querySelector("#productName").textContent = `${data[count].name}`;
             document.querySelector("#productPicture").src = `${data[count].image}`;
+            document.querySelector("#productPicture").width = "500";
+            document.querySelector("#productPicture").height = "500";
             document.querySelector("#productPrice").textContent = `$${data[count].price}`;
             document.querySelector("#productDescription").textContent = `${data[count].description}`;
+            Loading.style.display = `none`
         }).catch((err)=>{
             console.log(err);
             alert('There was an error loading products. Please try again soon.')  
